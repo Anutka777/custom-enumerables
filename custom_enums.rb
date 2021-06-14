e1bb8baec047f90a6db4610ed364bb39a28a57bd
Anna Baburina <4nna.baburina@gmail.com>
2021-06-14 10:36:01 +0500

    ADD solutions 3 - 6

--- custom_enums.rb
+++ custom_enums.rb
@@ -5,6 +5,7 @@ module Enumerable
   def my_each
     return puts 'ERROR: no block was given' unless block_given?
 
+    # No .each allowed!
     for item in self
       yield item
     end
@@ -14,9 +15,45 @@ module Enumerable
     return puts 'ERROR: no block was given' unless block_given?
 
     index = 0
+    # No .each allowed!
     for item in self
       yield item, index
       index += 1
     end
+  end
+
+  def my_select
+    return puts 'ERROR: no block was given' unless block_given?
+
+    data = []
+    my_each { |item| data.push item if yield item }
+    data
+  end
+
+  def my_all?
+    return puts 'ERROR: no block was given' unless block_given?
+
+    my_each do |item|
+      return false unless yield item
+    end
+    true
+  end
+
+  def my_any?
+    return puts 'ERROR: no block was given' unless block_given?
+
+    my_each do |item|
+      return true if yield item
+    end
+    false
+  end
+
+  def my_none?
+    return puts 'ERROR: no block was given' unless block_given?
+
+    my_each do |item|
+      return false if yield item
+    end
+    true
   end
 end
--- test_scripts.rb
+++ test_scripts.rb
@@ -2,17 +2,51 @@
 
 require_relative 'custom_enums'
 
-puts 'my_each vs each'
 example = [1, 2, 3, 4, 5]
+
+puts 'my_each vs each'
+puts 'Good call:'
 example.my_each { |item| puts item }
+puts 'And now witohout block:'
 example.my_each
 puts ''
 example.each { |item| puts item }
 puts '--------------------'
 
 puts 'my_each_with_index vs each_with_index'
-example = [1, 2, 3, 4, 5]
 example.my_each_with_index { |item, index| puts "#{item} has index #{index}" }
 puts ''
 example.each_with_index { |item, index| puts "#{item} has index #{index}" }
+puts '--------------------'
+
+puts 'my_select vs select'
+p(example.my_select { |item| item < 3 })
+puts ''
+p(example.select { |item| item < 3 })
+puts '--------------------'
+
+puts 'my_all? vs all?'
+p(example.my_all? { |item| item < 3 })
+p(example.my_all? { |item| item < 6 })
+puts ''
+p(example.all? { |item| item < 3 })
+p(example.all? { |item| item < 6 })
+puts '--------------------'
+
+puts 'my_any? vs any?'
+p(example.my_any? { |item| item < 3 })
+p(example.my_any? { |item| item > 6 })
+p(example.my_any? { |item| item == 1 })
+puts ''
+p(example.any? { |item| item < 3 })
+p(example.any? { |item| item > 6 })
+p(example.any? { |item| item == 1 })
+puts '--------------------'
+
+puts 'my_none? vs none?'
+p(example.my_none? { |item| item < 3 })
+p(example.my_none? { |item| item > 6 })
+puts ''
+p(example.none? { |item| item < 3 })
+p(example.none? { |item| item > 6 })
 puts '--------------------'
